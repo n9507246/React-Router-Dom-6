7abc74f03a56566375c5fdee6fdeccc5b2cb0b3a
Nikita Balashov <noWorkingEmail@mail.ru>
2023-12-04 10:25:12 +0500

    компоненты Routes,Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom' 
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Blog from './pages/Blog/Blog'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
 
   return (
     <>
-
+      <Routes>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/about' element={ <About/> }/>
+        <Route path='/blog' element={ <Blog/> }/>
+        <Route path='*' element={ <NotFound/> }/>
+      </Routes>
     </>
   )
 }
