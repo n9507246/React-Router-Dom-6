f506195abb3f1392835b8b2c9bc997a07d2c6ff2
Nikita Balashov <noWorkingEmail@mail.ru>
2023-12-05 15:19:45 +0500

    навигация по истории. кнопка назад.

--- src/pages/PostShow/PostShow.jsx
+++ src/pages/PostShow/PostShow.jsx
@@ -1,26 +1,33 @@
-import {useParams} from 'react-router-dom'
+import {useParams, useNavigate} from 'react-router-dom'
 import {useState, useEffect} from 'react'
 
 export default function PostShow(){
     
     const {id} = useParams()
     
+    const navigate = useNavigate(); const PREVIOS_PAGE = -1; 
+    const isNotEmptyObject = (obj) => Object.keys(obj).length !== 0  
+    
     const [dataPost, setDataPost] = useState([])
     useEffect(()=>{
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res => res.json())
-            .then(data => setDataPost(data))
+        .then(res => res.json())
+        .then(data => setDataPost(data))
     }, [id])
 
-    if(dataPost == undefined) return(
+    
+    if(isNotEmptyObject(dataPost))
+     return(
         <>
-            <h1>{dataPost.title}</h1>
-            <p>{dataPost.body}</p>
+            <button onClick={() => { navigate(PREVIOS_PAGE)} } >
+                Go back
+            </button>
+            <div>
+                <h1>{dataPost.title}</h1>
+                <p>{dataPost.body}</p>
+            </div>
         </>
     )
-    else return(
-        <>
-            Публикация не найдена
-        </>
-    )
+
+
 }
